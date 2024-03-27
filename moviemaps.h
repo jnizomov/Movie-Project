fff67dadcd76120e15ac47b148148fafad8e1b8d
jnizomov <javlon121212@gmail.com>
2024-03-27 14:13:42 -0400

    Mar27

---
+++ moviemaps.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+struct movie{
+    string movieTitle;
+    string genre;
+    float rating;
+
+    movie(string title, string genre, float rate){
+        
+    }
+
+}
