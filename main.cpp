0c9d38225cecd184433b1b780c9913255a471e96
BillJesser <127352761+BillJesser@users.noreply.github.com>
2024-04-15 18:48:12 -0400

    Update main.cpp
    new main to test heap sort algo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 // #include <QApplication>
 // #include <QPushButton>
-
 #include "movieList.h"
 
 
@@ -14,10 +13,20 @@ int main(/*int argc, char **argv */)
 //  return app.exec();
 
     MovieList list = MovieList("movies.csv", "ratings.csv");
-    //prints the first 100 movies
-    for (int i = 0; i < 100; i++)
-        list.printMovie(i);
-
+    //Uses a heap sort to modify the movie list.
+    list.heapSort();
+    //prints the top 100 highest reviewed films with a minimum number of ratings for the user input.
+    int minRate;
+    cout << "What are the minimum number of ratings you would like for the movies displayed to have?" << endl;
+    cin >> minRate;
+    int count = 0;
+    int i = 0;
+    while (count < 100 && list.movieExist(i)) {
+        if (list.getNumRatings(i) >= minRate) {
+            list.printMovie(i);
+            count++;
+        }
+        i++;
+    }
     return 0;
 }
-
