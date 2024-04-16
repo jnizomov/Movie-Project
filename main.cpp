aacff8250dc063cccc310eb8e2025007494c0451
BillJesser <127352761+BillJesser@users.noreply.github.com>
2024-04-16 12:12:42 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,51 @@
-// #include <QApplication>
-// #include <QPushButton>
 #include "movieList.h"
 
 
-int main(/*int argc, char **argv */)
+int main()
 {
-//  QApplication app (argc, argv);
-
-//  QPushButton button ("Hello world !");
-//  button.show();
-
-//  return app.exec();
-
     MovieList list = MovieList("movies.csv", "ratings.csv");
-    //Uses a heap sort to modify the movie list.
-    list.heapSort();
-    //prints the top 100 highest reviewed films with a minimum number of ratings for the user input.
-    int minRate;
-    cout << "What are the minimum number of ratings you would like for the movies displayed to have?" << endl;
-    cin >> minRate;
-    int count = 0;
-    int i = 0;
-    while (count < 100 && list.movieExist(i)) {
-        if (list.getNumRatings(i) >= minRate) {
-            list.printMovie(i);
-            count++;
+
+    bool cont = true;
+    string sortAlgo;
+
+    cout << "Welcome to Movie Sorting Algorithms: The program that allows users to sort movies based on ratings." << endl;
+
+    while (cont) {
+        cout<< "What sorting algorithm would you like to use? (quick, heap, merge)" << endl;
+        cin >> sortAlgo;
+        if (sortAlgo == "quick"){
+            cout<< "quicksort" << endl; // replace this line with a call to quick sort algo
+        } else if (sortAlgo == "heap") {
+            list.heapSort();
+            //print time taken to complete sorting algo
+        } else if (sortAlgo == "merge") {
+            cout << "mergesort" << endl;  // replace this line with a call to merge sort
+        } else {
+            cout << "Invalid Input" << endl;
+            continue;
         }
-        i++;
+        //prints the top 100 highest reviewed films with a minimum number of ratings for the user input.
+        int minRate;
+        cout << "What are the minimum number of ratings you would like for the movies displayed to have?" << endl;
+        cin >> minRate;
+        int count = 0;
+        int i = 0;
+        while (count < 100 && list.movieExist(i)) {
+            if (list.getNumRatings(i) >= minRate) {
+                list.printMovie(i);
+                count++;
+            }
+            i++;
+        }
+
+        char keepGoing;
+        cout << "Would you like to try a different sorting algorithm? (y for yes, any other character for no)" << endl;
+        cin >> keepGoing;
+        if (keepGoing == 'y'){
+            list.resetList();
+        } else {
+            cont = false;
+        }
     }
     return 0;
 }
